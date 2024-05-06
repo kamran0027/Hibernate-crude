af2818e1dc0cab70cc28573b67d791d9dbead38b
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-07 00:38:55 +0530

    testing the configyration

--- src/main/java/org/kamran/Main.java
+++ src/main/java/org/kamran/Main.java
@@ -1,8 +1,17 @@
 package org.kamran;
 
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        try (SessionFactory factory = HibernateUtil.getSessionFactory()) {
+            System.out.println(factory);
+            System.out.println("connection complete");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
