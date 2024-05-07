8449cc2a69eac5fba61d32298833660d7e00cc1e
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-07 23:45:02 +0530

    create a student class and use annotation

---
+++ src/main/java/org/kamran/Student.java
@@ -0,0 +1,69 @@
+package org.kamran;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    private int id;
+    private String name;
+    private String city;
+    private int age;
+
+    public Student(int id, String name, String city, int age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+    public Student(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
