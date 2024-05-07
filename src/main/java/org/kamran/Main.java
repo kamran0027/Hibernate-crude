24fc3cd1e6c84d3468fc7bd272112b6848425450
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-08 00:35:37 +0530

    create table and insert value in it

--- src/main/java/org/kamran/Main.java
+++ src/main/java/org/kamran/Main.java
@@ -1,16 +1,33 @@
 package org.kamran;
-
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        try (SessionFactory factory = HibernateUtil.getSessionFactory()) {
+        try {
+            SessionFactory factory = HibernateUtil.getSessionFactory();
             System.out.println(factory);
-            System.out.println("connection complete");
-        }
-        catch (Exception e){
+
+
+            Student st=new Student();
+            st.setId(103);
+            st.setName("adarsh");
+            st.setCity("grugaon");
+            st.setAge(20);
+
+            System.out.println(st);
+
+            Session session=factory.openSession();
+            Transaction tx=session.beginTransaction();
+
+            session.save(st);
+            tx.commit();
+            session.close();
+            System.out.println("transaction completed");
+        } catch (HibernateException e) {
             System.out.println(e.getMessage());
         }
     }
