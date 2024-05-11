5c779334327a98b24b261d6b0aa89c3539e3ab4f
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-12 01:41:01 +0530

    we create a Question class entity

---
+++ src/main/java/org/maping/Question.java
@@ -0,0 +1,47 @@
+package org.maping;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Question {
+    @Id
+    private int QuestionId;
+    @OneToOne
+    private Answer answer;
+
+    public Question(int questionId, String question) {
+        QuestionId = questionId;
+        this.question = question;
+    }
+
+    public Question() {
+    }
+
+    private String question;
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public int getQuestionId() {
+        return QuestionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        QuestionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
