d911e026d758fe56701ef2b654a9423d1e2d00d3
Kamran Ahmad <kamranahmadmd463@gmail.com>
2024-05-12 01:41:46 +0530

    we create a Answer class entity and map with question by oneToOne maping

---
+++ src/main/java/org/maping/Answer.java
@@ -0,0 +1,37 @@
+package org.maping;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Answer {
+    @Id
+    private int answerid;
+
+    private  String answer;
+
+    public Answer(int answerid, String answer) {
+        this.answerid = answerid;
+        this.answer = answer;
+    }
+
+    public Answer() {
+
+    }
+
+    public int getAnswerid() {
+        return answerid;
+    }
+
+    public void setAnswerid(int answerid) {
+        this.answerid = answerid;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
